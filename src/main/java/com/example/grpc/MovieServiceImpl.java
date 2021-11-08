a9c5ede3e91bbc88447799f70753df92c4cca954
zhiiw <wzy5363@gmail.com>
2021-11-08 09:27:47 +0800

    update

--- src/main/java/com/example/grpc/MovieServiceImpl.java
+++ src/main/java/com/example/grpc/MovieServiceImpl.java
@@ -2,8 +2,5 @@ package com.example.grpc;
 import io.grpc.stub.StreamObserver;
 
 public class MovieServiceImpl extends MovieServiceGrpc.MovieServiceImplBase {
-    @Override
-    public void getMovieBasedRecommend(MovieRequest,MovieResponse){
 
-    }
 }
--- src/main/java/com/example/grpc/util/ImporterToRedis.java
+++ src/main/java/com/example/grpc/util/ImporterToRedis.java
@@ -8,23 +8,7 @@ import java.util.Scanner;
 
 public class ImporterToRedis {
     public static void main(String[] args) throws FileNotFoundException {
-        Scanner sc = new Scanner(new File("/home/zhiiw/文档/ml-25m/ratings.csv"));
-        //parsing a CSV file into the constructor of Scanner class
-        Jedis jedis = new Jedis("127.0.0.1",6379);
-        sc.next();
-        sc.useDelimiter(",");
-        //setting comma as delimiter pattern
-        int x=0;
-        while (sc.hasNext()) {
-            if(x>=20000){
-                break;
-            }
-            jedis.hmset();
 
-            x++;
-        }
-        sc.close();
-        System.out.println(jedis.get("113"));
     }
     //tagId -> tag
     //用户看过的电影 加起来
